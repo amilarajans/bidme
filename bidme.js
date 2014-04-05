c89aae2bf6fc228b74ff0fd581eb10bb52ee3828
Amila Kumara <amilarajans@gmail.com>
2014-04-05 22:12:10 +0530

    login complete

--- bidme.js
+++ bidme.js
@@ -1,27 +1,71 @@
-Logininfo=new Meteor.Collection('logininfo');
+Logininfo = new Meteor.Collection('logininfo');
 
 if (Meteor.isClient) {
-    // Template.hello.greeting = function() {
-    //     return "Welcome to bidme. " + Session.get("greet");
-    // };
 
-    // Template.hello.events({
-    //     'keypress input': function(e, t) {
-    //         if (e.keyCode == 13) {
-    //             console.log('Welcome ' + t.find('#intxt').value);
-    //             Session.set("greet",  t.find('#intxt').value);
-    //         };
-    //     }
-    // });
-
-  Template.login.events({
+    Template.login.events({
         'submit .form-signin': function(e, t) {
-            // if (e.keyCode == 13) {
-                console.log('email - ' + t.find('#email').value);
-                console.log('password - ' + t.find('#password').value);
-                Logininfo.insert({username:t.find('#email').value,password:t.find('#password').value});
-                
-            // };
+            // events.preventDefault();
+            console.log('username - ' + t.find('#loginForm #username').value);
+            console.log('password - ' + t.find('#loginForm #password').value);
+            alert('login');
+            // Logininfo.insert({
+            //     username: t.find('#username').value,
+            //     password: t.find('#password').value
+            // });
+            var username = t.find('#loginForm #username').value;
+            var password = t.find('#loginForm #password').value;
+            Meteor.loginWithPassword(username, password, function(err) {
+                if (err)
+                    alert('failed');
+                else
+                    alert('logged');
+            });
+        }
+    });
+
+    Template.signup.events({
+        'submit .form-signin': function(e, t) {
+            // events.preventDefault();
+            console.log('username - ' + t.find('#signupForm #username').value);
+            console.log('password - ' + t.find('#signupForm #password').value);
+            console.log('name - ' + t.find('#signupForm #name').value);
+            console.log('email - ' + t.find('#signupForm #email').value);
+            console.log('mobile - ' + t.find('#signupForm #mobile').value);
+            alert('signup');
+            var username = t.find('#signupForm #username').value;
+            var password = t.find('#signupForm #password').value;
+            var name = t.find('#signupForm #name').value;
+            var email = t.find('#signupForm #email').value;
+            var mobile = t.find('#signupForm #mobile').value;
+            Accounts.createUser({
+                username: username,
+                password: password,
+                profile: {
+                    name: name,
+                    email:email,
+                    mobile:mobile
+                }
+            }, function(err) {
+                if (err) {
+                    alert('account creation failed');
+                } else {
+                    alert('account created');
+                }
+
+            });
+        }
+    });
+
+    Meteor.autorun(function() {
+        // Whenever this session variable changes, run this function.
+        var message = Session.get('displayMessage');
+        if (message) {
+            var stringArray = message.split('&amp;');
+            ui.notify(stringArray[0], stringArray[1])
+                .effect('slide')
+                .closable();
+
+            Session.set('displayMessage', null);
         }
     });
 }
